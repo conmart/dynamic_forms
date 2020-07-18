ce9f7128ad87b745fa63b254fdac5ed3a2784d14
conmart <connor.a.martinelli@gmail.com>
2020-07-17 17:28:42 -0700

    working draft

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { data } from './exampleData';
+import Form from './Form';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formResults: '',
+    };
+  }
+
+  setFormResults = (formResults) => {
+    this.setState({ formResults });
+  };
+
+  render() {
+    const { formResults } = this.state;
+    return (
+      <div className="App">
+        <h1>Dynamic Forms</h1>
+        <Form data={data} saveResults={this.setFormResults} />
+
+        <div className="results">{formResults}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const buildFormState = (data) => {
+  const state = {};
+  for (let input of data) {
+    const defaultValue = input['type'] === 'checkbox' ? false : '';
+    state[input['name']] = defaultValue;
+  }
+  return state;
+};
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    const formState = buildFormState(this.props.data);
+    this.state = {
+      formState,
+    };
+  }
+
+  changeInput = (e) => {
+    const { formState } = this.state;
+    const newFormState = { ...formState };
+    const inputValue = e.target.type === 'checkbox'
+      ? e.target.checked
+      : e.target.value;
+    newFormState[e.target.name] = inputValue;
+    this.setState({ formState: newFormState });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { saveResults } = this.props;
+    const { formState } = this.state;
+    const JSONData = JSON.stringify(formState);
+    saveResults(JSONData);
+  };
+
+  render() {
+    const { data } = this.props;
+    const { formState } = this.state;
+
+    return (
+      <form className="form">
+        {data.map((field) => {
+          let showField = true;
+          const conditional = field['conditional'];
+          if (conditional) {
+            const fn = conditional['show_if'];
+            const conditionalValue = formState[conditional['name']];
+            showField = fn(conditionalValue);
+          }
+          if (showField) {
+            return (
+              <div className="inputWrapper" key={field['name']}>
+                <label>{field['human_label']}</label>
+                <input
+                  type={field['type']}
+                  name={field['name']}
+                  onChange={this.changeInput}
+                  value={formState[field['name']]}
+                />
+              </div>
+            );
+          }
+        })}
+        <button onClick={this.handleSubmit} type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
